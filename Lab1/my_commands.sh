a977893ff94a63a751fded2cd84601fee50fd4fb
“theshermanator” <dovindustries@gmail.com>
2019-09-13 10:11:40 -0400

    adding stuff

--- Lab1/my_commands.sh
+++ Lab1/my_commands.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 ./echo_arg csc209 > echo_out.txt 
 ./echo_stdin < echo_stdin.c
 ./count 210 > echo_out.txt 
 wc -c < echo_out.txt  
-ls -S | head -1 < ./echo_stdin
+ls -1S | head -n1 < ./echo_stdin
