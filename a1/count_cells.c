f5e7f58ea9a054ace565af332b90c9a4b1d5f557
Markus <markus@markus.com>
2019-09-23 21:41:18 -0400

    Update starter code files for assignment a1

---
+++ a1/count_cells.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_image(int num_rows, int num_cols, 
+                int arr[num_rows][num_cols], FILE *fp);
+                
+void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]);
+
+// Remember to include the function prototype for count_cells
+
+
+int main(int argc, char **argv) {
+    // Print a message to stderr and exit with an argument of 1 if there are
+    // not the right number of parameters, or the second argument is not -p
+
+
+    return 0;
+}
---
+++ a1/image.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    
+/* Reads the image from the open file fp into the two-dimensional array arr 
+ * num_rows and num_cols specify the dimensions of arr
+ */
+void read_image(int num_rows, int num_cols, 
+                int arr[num_rows][num_cols], FILE *fp) {
+
+}
+
+/* Print to standard output the contents of the array arr */
+void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
+
+}
+
+/* TODO: Write the count_cells function */
