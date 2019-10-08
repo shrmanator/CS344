bf0f816f9054f219265546d41e0008a338d7e8f4
“theshermanator” <dovindustries@gmail.com>
2019-10-08 14:17:30 -0400

    Added partially completed count_cells.c and image.c.

--- a1/count_cells.c
+++ a1/count_cells.c
@@ -1,18 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define LINE_LENGTH 80
 
-void read_image(int num_rows, int num_cols, 
-                int arr[num_rows][num_cols], FILE *fp);
-                
+
+void read_image(int num_rows, int num_cols, int arr[num_rows][num_cols], FILE *fp);
+
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
-// Remember to include the function prototype for count_cells
-
+void count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
 int main(int argc, char **argv) {
-    // Print a message to stderr and exit with an argument of 1 if there are
-    // not the right number of parameters, or the second argument is not -p
+    FILE *text;
+    char rows_cols_string[LINE_LENGTH + 1]; // This will be converted to an int in num_rows.
+    // Open text file for reading:
+    text = fopen(argv[1], "r");
+    
+    //Error checking:
+    if (text == NULL || argc < 1 || argc > 3) {
+        fprintf(stderr, "Error opening A2.txt\n");
+        exit(1);
+    }
+    if (argv[2] != '\0') {
+        if (strcmp(argv[2], "-p") != 0) {
+            fprintf(stderr, "Second arg must be -p.\n");
+            exit(1);
+        }
+    }
+    
+    // Getting num of rows and cols, but it's a string:
+    fgets(rows_cols_string, LINE_LENGTH, text);
+    // Time to convert to ints:
+    char rows_str[LINE_LENGTH];
+    char cols_str[LINE_LENGTH];
+    int i = 0;
+    while(rows_cols_string[i] != ' ') {
+        rows_str[i] = rows_cols_string[i];
+        i++;
+    }
+    int j = 0;
+    while(rows_cols_string[j] != '\n') {
+        cols_str[j] = rows_cols_string[i + 1];
+        j++;
+        i++;
+    }
+    //now we convert:
+    int rows_int = atoi(rows_str);
+    int cols_int = atoi(cols_str);
+    
+    // Initializing the resulting array to be filled with contents of txt file.
+    int text_file_array[rows_int][cols_int];
 
+    read_image(rows_int, cols_int, text_file_array, text);
+    if (strcmp(argv[2], "-p") == 0) {
+        print_image(rows_int, cols_int, text_file_array);
+    }
+    count_cells(rows_int, cols_int, text_file_array);
 
+    // Manipulation completed. Close A2.txt:
+    if (fclose(text) != 0) {
+        fprintf(stderr, "fclose failed\n");
+        return 1;
+    }
     return 0;
 }
--- a1/image.c
+++ a1/image.c
@@ -1,16 +1,59 @@
 #include <stdio.h>
+#include <string.h>
     
-/* Reads the image from the open file fp into the two-dimensional array arr 
+/* Reads the image from the open file fp into the two-dimensional array arr
  * num_rows and num_cols specify the dimensions of arr
  */
-void read_image(int num_rows, int num_cols, 
-                int arr[num_rows][num_cols], FILE *fp) {
-
+void read_image(int num_rows, int num_cols, int arr[num_rows][num_cols], FILE *fp) {
+    // NOT READING FIRST ITEM IN TXT FILE!
+    for (int i=0;i<num_rows;i++) {
+        for (int j=0; j<num_cols; j++) {
+            fscanf(fp, "%d", &arr[i][j]);
+        }
+    }
 }
 
 /* Print to standard output the contents of the array arr */
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
-
+    for (int i=0; i<num_rows; i++) {
+            for (int j=0; j<num_cols; j++) {
+                printf(" %d", arr[i][j]);
+            }
+    }
 }
 
 /* TODO: Write the count_cells function */
+void count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]){
+    int single_cell_size = 0;
+    int total_cells = 0;
+    int visitied = 0;
+    
+    for (int i=0; i<num_cols; i++) {
+        for (int j=0; j<num_rows; j++) {
+            if (arr[i][j] == 225) {
+                arr[i][j] = -1;
+                if (i - 1 < 0) {
+                    // the value above
+                    if (arr[i - 1][j] != 0) {
+                        arr[i-1][j] = -1;
+                        visitied = 1;
+                        }
+                }
+                if (i + 1 > num_rows) {
+                    // the value below
+                    if (arr[i + 1][j] != 0) {
+                        arr[i+1][j] = -1;
+                        visitied = 1;
+                        
+                    // the value left
+                    // the value right
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+    
+       
