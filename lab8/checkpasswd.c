acd33c8ff296fb6b91a9817ac5dec4299af51e05
“theshermanator” <dovindustries@gmail.com>
2019-10-31 12:55:51 -0400

    0.00. Initial commit. (+ fixed indentation)

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -12,25 +12,26 @@
 #define NO_USER "No such user\n"
 
 int main(void) {
-  char user_id[MAXLINE];
-  char password[MAXLINE];
+    char user_id[MAXLINE];
+    char password[MAXLINE];
 
-  /* The user will type in a user name on one line followed by a password 
-     on the next.
-     DO NOT add any prompts.  The only output of this program will be one 
-	 of the messages defined above.
-   */
+    /* The user will type in a user name on one line followed by a password
+    on the next.
+    DO NOT add any prompts.  The only output of this program will be one
+    of the messages defined above.
+    */
 
-  if(fgets(user_id, MAXLINE, stdin) == NULL) {
-      perror("fgets");
-      exit(1);
-  }
-  if(fgets(password, MAXLINE, stdin) == NULL) {
-      perror("fgets");
-      exit(1);
-  }
-  
-  // TODO
+    if(fgets(user_id, MAXLINE, stdin) == NULL) {
+        perror("fgets");
+        exit(1);
+    }
+    if(fgets(password, MAXLINE, stdin) == NULL) {
+        perror("fgets");
+        exit(1);
+    }
 
-  return 0;
+    // TODO
+    
+
+    return 0;
 }
