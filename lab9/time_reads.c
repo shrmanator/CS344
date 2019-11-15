cc16992bad0c2b16f0d0cb17fc49e06beb8c58d5
“theshermanator” <dovindustries@gmail.com>
2019-11-14 23:16:32 -0500

    1.01 Part 3 complete

--- lab9/time_reads.c
+++ lab9/time_reads.c
@@ -15,12 +15,18 @@
  */
 long num_reads, seconds;
 
+/*
+ It's a handler.
+ */
+void handler(int code) {
+    fprintf(stderr, "it ded");
+    exit(0);
+}
 
 /* The first command-line argument is the number of seconds to set a timer to run.
  * The second argument is the name of a binary file containing 100 ints.
  * Assume both of these arguments are correct.
  */
-
 int main(int argc, char **argv) {
     if (argc != 3) {
         fprintf(stderr, "Usage: time_reads s filename\n");
@@ -33,15 +39,24 @@ int main(int argc, char **argv) {
       perror("fopen");
       exit(1);
     }
-
+    
+    struct sigaction siggy;
+    siggy.sa_handler = handler;
+    siggy.sa_flags = 0;             // default flags
+    sigemptyset(&siggy.sa_mask);   // don't block signals during handler
+    sigaction(SIGPROF, &siggy, NULL);
+    
     /* In an infinite loop, read an int from a random location in the file,
      * and print it to stderr.
      */
     for (;;) {
-
-
-
-
+        int j, value;
+        j = random() % 100;
+        fseek(fp, j * sizeof(int), SEEK_SET);
+        value = read(fileno(fp), &value, 1);
+        fprintf(stderr, "%d", value);
     }
+    
+    
     return 1; // something is wrong if we ever get here!
 }
